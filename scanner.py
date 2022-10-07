24b8be20ba9990f546bad1f161aac36eb52d6290
Suzal Regmi <sregmi2@gsu.edu>
2022-10-07 17:56:09 -0400

    this works too

--- scanner.py
+++ scanner.py
@@ -1,8 +1,9 @@
 import cv2 #Read image / camera/video input
+from tabulate import tabulate
 from pyzbar.pyzbar import decode
+import sys
+import csv
 
-
-dict = {"Manufacturer Name"}
 used_codes = []
 cap = cv2.VideoCapture (0)
 cap.set (3, 640) #3 - Width
@@ -10,25 +11,34 @@ cap.set (4, 480) #4 - Height
 camera = True
 while camera == True:
     success, frame = cap.read()
-    try:
+    
+    with open (sys.argv[1], "a") as file:
+        writer = csv.DictWriter (file, fieldnames=["Manufacturer Name", "S/N", "MDL"])
+        #writer.writeheader()
+
         for code in decode(frame) :
             if code.data.decode('utf-8') not in used_codes:
-                used_codes.append(code.data.decode('utf-8'))
                 if code.type == "CODE128":
                     test = code.data.decode('utf-8')
-                    HDD, S_N = test.split("-")
-                    if S_N == "":
-                        print (f"S/N: {code.data.decode('utf-8')}")
+                    try:
+                        HDD, S_N = test.split("-")
+                    except (ValueError):
+                        used_codes.append(test)
+                        pass
                     else:
+                        used_codes.append(test)
+                        used_codes.append(S_N)
                         if HDD == "WD":
                             MN = "Western Digital"
-                            print (f"Manufacturer: {MN}")
-                        print (f"S/N: {S_N}")
+                  
                 if code.type == "CODE39":
-                    print (f"MDL: {code.data.decode('utf-8')}")
-    except:
-        pass
-        
-        
+                    MDL = code.data.decode('utf-8')
+                    used_codes.append(MDL)
+
+                try:
+                    writer.writerow({"Manufacturer Name":MN, "S/N":S_N, "MDL":MDL})
+                except (NameError):
+                    pass
+                        
     cv2.imshow('Testing-code-scan', frame)
     cv2.waitKey(1)
--- test.py
+++ test.py
@@ -5,15 +5,17 @@ import sys
 import csv
 
 used_codes = []
-dict= {}
 cap = cv2.VideoCapture (0)
 cap.set (3, 640) #3 - Width
 cap.set (4, 480) #4 - Height
 camera = True
 while camera == True:
     success, frame = cap.read()
+    
     with open (sys.argv[1], "a") as file:
         writer = csv.DictWriter (file, fieldnames=["Manufacturer Name", "S/N", "MDL"])
+        #writer.writeheader()
+
         for code in decode(frame) :
             if code.data.decode('utf-8') not in used_codes:
                 if code.type == "CODE128":
@@ -21,21 +23,22 @@ while camera == True:
                     try:
                         HDD, S_N = test.split("-")
                     except (ValueError):
-                        S_N = test
-                        writer.writerow({"S/N":S_N})
-                        used_codes.append(S_N)
+                        used_codes.append(test)
+                        pass
                     else:
+                        used_codes.append(test)
+                        used_codes.append(S_N)
                         if HDD == "WD":
                             MN = "Western Digital"
-                            writer.writerow({"Manufacturer Name":MN, "S/N":S_N})
-                        used_codes.append(S_N)
-                    
+                  
                 if code.type == "CODE39":
                     MDL = code.data.decode('utf-8')
-                    writer.writerow({"MDL":MDL})
                     used_codes.append(MDL)
 
-                
-
-        cv2.imshow('Testing-code-scan', frame)
-        cv2.waitKey(1)
+                try:
+                    writer.writerow({"Manufacturer Name":MN, "S/N":S_N, "MDL":MDL})
+                except (NameError):
+                    pass
+                        
+    cv2.imshow('Testing-code-scan', frame)
+    cv2.waitKey(1)
