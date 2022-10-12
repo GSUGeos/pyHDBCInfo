f47250c91ea370e4b27f1e7d15e7f67703d85aab
Suzal Regmi <sregmi2@gsu.edu>
2022-10-12 18:56:41 -0400

    final commit before the working code

--- test.py
+++ test.py
@@ -1,38 +1,43 @@
 import cv2 #Read image / camera/video input
 from pyzbar.pyzbar import decode
-
-
-
+import sys
+import csv
 
 used_codes = []
-
-def main():
+cap = cv2.VideoCapture (0)
+cap.set (3, 640) #3 - Width
+cap.set (4, 480) #4 - Height
+camera = True
+while camera == True:
+    success, frame = cap.read()
     
-    
-    print (getS_N())
+    with open (sys.argv[1], "a") as file:
+        writer = csv.DictWriter (file, fieldnames=["Manufacturer Name", "S/N", "MDL"])
+        #writer.writeheader()
 
-    
-
-def getS_N():
-    unfilteredS_N = []
-    unfiltered_MDL = []
-    global used_codes
-    cap = cv2.VideoCapture (0)
-    cap.set (3, 640) #3 - Width
-    cap.set (4, 480) #4 - Height
-    camera = True
-
-    while camera == True:
-        success, frame = cap.read()
         for code in decode(frame) :
             if code.data.decode('utf-8') not in used_codes:
-                used_codes.append(code.data.decode('utf-8'))
                 if code.type == "CODE128":
-                    unfilteredS_N.append(code.data.decode('utf-8'))
+                    test = code.data.decode('utf-8')
+                    try:
+                        HDD, S_N = test.split("-")
+                    except (ValueError):
+                        used_codes.append(test)
+                        pass
+                    else:
+                        used_codes.append(test)
+                        used_codes.append(S_N)
+                        if HDD == "WD":
+                            MN = "Western Digital"
+                  
                 if code.type == "CODE39":
-                    unfiltered_MDL.append(code.data.decode('utf-8'))
-        if len(unfiltered_MDL) == 1 and len(unfilteredS_N) == 1:
-            return unfilteredS_N, unfiltered_MDL
-            
-if __name__ == "__main__":
-    main()
+                    MDL = code.data.decode('utf-8')
+                    used_codes.append(MDL)
+
+                try:
+                    writer.writerow({"Manufacturer Name":MN, "S/N":S_N, "MDL":MDL})
+                except (NameError):
+                    pass
+                        
+    cv2.imshow('Testing-code-scan', frame)
+    cv2.waitKey(1)
